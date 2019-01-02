ec2ec614ede7872c44e8dee575eefe5cd0e92dff
Spencer Lyon <spencerlyon2@gmail.com>
2019-01-02 08:14:10 -0500

    ENH: add matplotlib and scipy as dependencies

--- setup.py
+++ setup.py
@@ -77,5 +77,5 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip"s
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["pandas", "requests", "quandl"],
+    install_requires=["pandas", "requests", "quandl", "scipy", "matplotlib"],
 )
